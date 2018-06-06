737de4c80fe7fbff7df2735dcf283a30b56ac5c8
Eugene <skullet-on@mail.ru>
2018-06-06 17:57:10 +0300

    do small fixes

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,17 +1,17 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
-let App = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const app = require('../app');
 
 chai.use(chaiHttp);
 
 describe('/GET app', () => {
 	it('it should GET status is server up', (done) => {
 		chai.request(app)
-			.get('/api/status')
+			.get('api/status')
 			.end((err, res) => {
 				res.should.have.status(200);
-				res.body.should.be.a('hash');
+				res.body.should.be.a('object');
 				done();
 			});
 	});
