2bc61393fa9c9e94b24fcde0392ab80be9123d25
Eugene <skullet-on@mail.ru>
2018-06-15 12:04:05 +0300

    added create News

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const models = require('../db/models').db;
+const news = require('./routes/news');
 
 app.get('/api/status', (req, res) => {
 	res.send({status: "server up"});
 });
 
-app.get('/api/news', (req, res) => {
-	models.News.findAll().then((news) => {
-		res.json(news);
-	});
-});
+app.use('/api/news', news);
 
 app.listen(port, () => {
 	console.log('Server is listening on port ' + port);
---
+++ src/routes/news.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../../db/models').db;
+const bodyparser = require('body-parser');
+
+router.use(bodyparser.json());
+
+router.get('/', (req, res) => {
+	models.News.findAll().then((news) => {
+		res.json(news);
+	});
+});
+
+router.post('/', (req, res) => {
+	const title = req.body.title;
+	const text = req.body.text;
+	models.News.create({
+		title: req.body.title,
+		text: req.body.text,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	})
+	.then(newNews => {
+		res.json(newNews);
+	})
+});
+
+module.exports = router;
