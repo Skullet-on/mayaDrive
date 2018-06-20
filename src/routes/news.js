67d017fe553cdfd0228e2915158ab8916fa85bae
Eugene <skullet-on@mail.ru>
2018-06-20 10:14:29 +0300

    fixed db

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {News} = require('../models').db;
+const {News} = require('../models');
 
 router.get('/', (req, res) => {
 	News
