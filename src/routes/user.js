c1d427273f9f6603a4c7bbb084fc4c13ccfcef64
Eugene <skullet-on@mail.ru>
2018-08-10 17:27:40 +0300

    add JWT and pesponse token

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {User} = require('../models');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
   User
@@ -29,5 +30,12 @@ router.post('/', (req, res) => {
     })
 });
 
+router.post('/login', (req, res) => {
+  jwt.sign({user: req.user}, 'secretkey', (err, token) => {
+    res.json({
+      token: token
+    })
+  })
+});
 
 module.exports = router;
