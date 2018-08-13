a7f4b351197a09f355ef3c5e76d98177d43987b4
Eugene <skullet-on@mail.ru>
2018-08-13 16:23:33 +0300

    Return token if email and password is match

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,17 +6,18 @@ const bcrypt = require("bcrypt");
 
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
-  
-  User.findOne({ where: { email } })
+
+  User.findOne({ where: { email: email.toLowerCase() } })
     .then(user => bcrypt.compare(password, user.password))
     .then(match => {
       if (!match) return res.status(401).json("Wrong Email or Password");
       jwt.sign({user: req.user}, 'secretkey', (err, token) => {
-        res.json({
+        res.status(200).json({
           token: token
         })
       })
     })
+    .catch(err => { res.status(500).json("Couldn't find this account") })
 });
 
 module.exports = router;
